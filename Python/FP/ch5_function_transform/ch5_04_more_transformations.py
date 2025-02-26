3ddaf234291b4b31cfe698bdd6786bdc53f176b9
Nongrenost <Nongrenost@gmail.com>
2025-02-26 14:06:07 +0300

    more fp stuff

--- Python/FP/ch5_function_transform/ch5_04_more_transformations.py
+++ Python/FP/ch5_function_transform/ch5_04_more_transformations.py
@@ -1,5 +1,10 @@
 def doc_format_checker_and_converter(conversion_function, valid_formats):
-    
+    def inner_func(filename, content):
+        if filename.split('.')[1] in valid_formats:
+            return conversion_function(content)
+        else:
+            raise ValueError("invalid file format")
+    return inner_func
 
 
 # Don't edit below this line
