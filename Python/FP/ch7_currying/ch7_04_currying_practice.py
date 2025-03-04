03b9b601981186e7bea8c2796abaeefed93a5890
Nongrenost <Nongrenost@gmail.com>
2025-03-04 11:33:05 +0300

    more fp stuff

---
+++ Python/FP/ch7_currying/ch7_04_currying_practice.py
@@ -0,0 +1,13 @@
+from functools import reduce
+
+def lines_with_sequence(char):
+    def with_char(length):
+        sequence = char * length
+        def with_length(doc):
+            lines = doc.split("\n")
+            return reduce(
+                lambda count, line: count + (1 if sequence in line else 0),
+            lines, 0)
+        return with_length
+    return with_char
+
