ffee582f9946b0b91c8d7eb1e64026c2726257db
Nongrenost <Nongrenost@gmail.com>
2025-06-05 07:31:22 +0300

    sorting algo l2

--- Python/DSA/ch4_sorting_algorithms/l2_bubble_sort.py
+++ Python/DSA/ch4_sorting_algorithms/l2_bubble_sort.py
@@ -1,2 +1,17 @@
 def bubble_sort(nums):
-    pass
+    if not nums:
+        return []
+    swapping = True
+    end = len(nums)
+    while(swapping):
+        swapping = False
+        for i in range(1, end):
+            if nums[i-1] > nums[i]:
+                hold = nums[i-1]
+                nums[i-1] = nums[i]
+                nums[i] = hold
+                swapping = True
+        end -= 1
+    return nums
+        
+        
