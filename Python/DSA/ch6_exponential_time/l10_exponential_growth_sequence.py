7311448cd569e7f754076f61bdb7539ff411c042
Nongrenost <Nongrenost@gmail.com>
2025-06-13 20:28:15 +0300

    ch6 l10

---
+++ Python/DSA/ch6_exponential_time/l10_exponential_growth_sequence.py
@@ -0,0 +1,8 @@
+def exponential_growth(n, factor, days):
+    followers_per_day = [n]
+    total = n
+    for day in range(0, days):
+        total = total ** factor
+        followers_per_day.append(total)
+    return followers_per_day
+    
