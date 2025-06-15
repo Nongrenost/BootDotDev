8994f29e4f150fb7446596a2653dbbca1712671d
Nongrenost <Nongrenost@gmail.com>
2025-06-15 10:22:39 +0300

    started data structures intro, l1

--- Python/DSA/ch5_exponential_time/l10_exponential_growth_sequence.py
+++ Python/DSA/ch5_exponential_time/l10_exponential_growth_sequence.py
@@ -2,7 +2,7 @@ def exponential_growth(n, factor, days):
     followers_per_day = [n]
     total = n
     for day in range(0, days):
-        total = total ** factor
+        total = total * factor
         followers_per_day.append(total)
     return followers_per_day
     
