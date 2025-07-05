81c4f0a8cc4afb700f0dd84422397c30619f69e4
Nongrenost <Nongrenost@gmail.com>
2025-07-05 16:30:18 +0300

    ch13 Tries l1

---
+++ Python/DSA/ch13_tries/l1_tries.py
@@ -0,0 +1,16 @@
+class Trie:
+    def add(self, word):
+        current = self.root
+        
+        for char in word:
+            if char not in current:
+                current[char] = {}
+            current = current[char]
+        current[self.end_symbol] = True
+                
+
+    # don't touch below this line
+
+    def __init__(self):
+        self.root = {}
+        self.end_symbol = "*"
