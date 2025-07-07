a6d2aa249f03f6b89e483c71c19e24028854aae7
Nongrenost <Nongrenost@gmail.com>
2025-07-07 13:31:55 +0300

    ch13 l6 find matches

---
+++ Python/DSA/ch13_tries/l6_find_matches.py
@@ -0,0 +1,30 @@
+class Trie:
+    def find_matches(self, document):
+        matches = set()
+        
+        for index in range(0, len(document)):
+            current_level = self.root
+            
+            for i in range(index, len(document)):
+                if document[i] not in current_level:
+                    break
+                current_level = current_level[document[i]]
+                if self.end_symbol in current_level:
+                    matches.add(''.join(document[index:i+1]))
+        return matches
+            
+
+
+    # don't touch below this line
+
+    def __init__(self):
+        self.root = {}
+        self.end_symbol = "*"
+
+    def add(self, word):
+        current = self.root
+        for letter in word:
+            if letter not in current:
+                current[letter] = {}
+            current = current[letter]
+        current[self.end_symbol] = True
