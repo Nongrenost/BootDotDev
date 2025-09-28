672b80d32a80b47ec12caf0e6c7c9d3449e21d8a
Nongrenost <Nongrenost@gmail.com>
2025-09-29 01:26:27 +0300

    refactoring

--- Go/Http-clients-Go/ch6_methods/l4_post_requests/http.go
+++ Go/Http-clients-Go/ch6_methods/l4_post_requests/http.go
@@ -11,20 +11,20 @@ import (
 func createUser(url, apiKey string, data User) (User, error) {
 	user, err := json.Marshal(data)
 	if err != nil {
-		return User{}, fmt.Errorf("error while marshaling json: ", err)
+		return User{}, fmt.Errorf("error while marshaling json: %w", err)
 	}
 
 
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(user))
 	if err != nil {
-		return User{}, fmt.Errorf("error while creating requst: ", err)
+		return User{}, fmt.Errorf("error while creating request: %w", err)
 	}
 
-	req.Header["Content-Type"] = append(req.Header["Content-Type"], "application/json" )
-	req.Header["X-API-key"] = append(req.Header["X-API-key"], apiKey)
+	req.Header.Set("Content-Type", "application/json")
+    req.Header.Set("X-API-Key", apiKey)
 	res, err := http.DefaultClient.Do(req) 
 	if err != nil {
-		return User{}, fmt.Errorf("error while getting a responce: ", err)
+		return User{}, fmt.Errorf("error while getting a response: %w", err)
 	}
 
 	defer res.Body.Close()
@@ -33,7 +33,7 @@ func createUser(url, apiKey string, data User) (User, error) {
 	decoder := json.NewDecoder(res.Body)
 	err = decoder.Decode(&respondUser)
 	if err != nil {
-		return User{}, fmt.Errorf("error while decoding json: ", err)
+		return User{}, fmt.Errorf("error while decoding json: %w", err)
 	}
 
 	return respondUser, nil
